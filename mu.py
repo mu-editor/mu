29426bc5f88b16290e8a6337ff1bdeb0f12b4f81
Nicholas H.Tollervey <ntoll@ntoll.org>
2015-12-07 20:22:39 +0000

    Add license and initial core code.

---
+++ mu.py
@@ -0,0 +1,84 @@
+"""
+Mu.py - a "micro" editor and exploration of a problem space.
+
+Copyright (c) 2015 Nicholas H.Tollervey.
+
+Based upon work done for Puppy IDE by Dan Pope, Nicholas Tollervey and Damien
+George.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import sys
+
+from PyQt4.QtWidgets import (QApplication, QSplashScreen, QStackedWidget,
+                             QDesktopWidget)
+from resources import load_icon, load_pixmap, load_stylesheet
+
+class Mu(QStackedWidget):
+    """
+    Represents the main application window.
+    """
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowIcon(load_icon('icon'))
+        self.update_title()
+
+        # Ensure we have a sensible size for the application.
+        self.setMinimumSize(800, 600)
+
+    def update_title(self, project=None):
+        """
+        Updates the title bar of the application.
+        """
+        title = "Mu Editor"
+        if project:
+            title += ' - ' + project
+        self.setWindowTitle(title)
+
+    def autosize_window(self):
+        """
+        Makes the editor 80% of the width*height of the screen and centres it.
+        """
+        screen = QDesktopWidget().screenGeometry()
+        w = int(screen.width() * 0.8)
+        h = int(screen.height() * 0.8)
+        self.resize(w, h)
+        size = self.geometry()
+        self.move(
+            (screen.width() - size.width()) / 2,
+            (screen.height() - size.height()) / 2
+        )
+
+
+if __name__ == '__main__':
+    # The app object is the application running on your computer.
+    app = QApplication(sys.argv)
+    app.setStyleSheet(load_stylesheet('mu.css'))
+
+    # A splash screen is a logo that appears when you start up the application.
+    # The image to be "splashed" on your screen is in the resources/images
+    # directory.
+    splash = QSplashScreen(load_pixmap('icon'))
+    splash.show()
+
+    # Make the editor with the Mu class defined above.
+    the_editor = Mu()
+    the_editor.show()
+    the_editor.autosize_window()
+
+    # Remove the splash when the_editor has finished setting itself up.
+    splash.finish(the_editor)
+
+    # Stop the program after the application finishes executing.
+    sys.exit(app.exec_())
