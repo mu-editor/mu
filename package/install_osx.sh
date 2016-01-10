2fa0ab4a3cdf311748dad34e75e4cce0f1a18d56
Peter Inglesby <peter.inglesby@gmail.com>
2016-01-10 13:23:01 +0000

    Add brew formula for installing QScintilla2 on OSX

--- package/install_osx.sh
+++ package/install_osx.sh
@@ -2,6 +2,6 @@
 set -ev
 brew update
 brew install python3
-sudo pip3 install --upgrade pip setuptools
 pip3 install pyinstaller
 brew install pyqt5 --with-python3
+brew install $(dirname "$PWD/${0#./}")/extras/qscintilla2.rb
