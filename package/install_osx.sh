90810b23b1b0ce7b1b977a7aeef8662834c1b21a
Peter Inglesby <peter.inglesby@gmail.com>
2016-01-13 21:00:40 +0000

    Use pip3 to install Pyinstaller, and back out verbose logging

--- package/install_osx.sh
+++ package/install_osx.sh
@@ -2,11 +2,6 @@
 set -ev
 brew update >/dev/null 2>&1  # This produces a lot of output that's not very interesting
 brew install python3
-sudo pip install git+git://github.com/inglesp/pyinstaller.git@e299fcb1d630c65e91019f6e6bff6fa3b87e8bc1
+sudo pip3 install git+git://github.com/inglesp/pyinstaller.git@f75edb9ac0da15ce0bdec52ff3aaef74aab5a470
 brew install pyqt5 --with-python3
-python3 -c 'import PyQt5; print(PyQt5.__file__)'
-python3 -c 'import PyQt5.QtWidgets; print(PyQt5.QtWidgets.__file__)'
 brew install $(dirname "$PWD/${0#./}")/extras/qscintilla2.rb
-python3 -c 'import PyQt5; print(PyQt5.__file__)'
-python3 -c 'import PyQt5.QtWidgets; print(PyQt5.QtWidgets.__file__)'
-python3 -c 'import PyQt5.Qsci; print(PyQt5.Qsci.__file__)'
