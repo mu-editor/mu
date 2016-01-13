3256ab5d9c14391b6f8c4efebf39b7ec43d245ef
Peter Inglesby <peter.inglesby@gmail.com>
2016-01-13 21:53:47 +0000

    Install PyInstaller with pip3 on OSX

--- package/install_osx.sh
+++ package/install_osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
-brew update
+brew update >/dev/null 2>&1  # This produces a lot of output that's not very interesting
 brew install python3
-sudo pip install git+git://github.com/inglesp/pyinstaller.git@f75edb9ac0da15ce0bdec52ff3aaef74aab5a470
+sudo pip3 install git+git://github.com/inglesp/pyinstaller.git@f75edb9ac0da15ce0bdec52ff3aaef74aab5a470
 brew install pyqt5 --with-python3
 brew install $(dirname "$PWD/${0#./}")/extras/qscintilla2.rb
