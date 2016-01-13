bfc5dafad811bac1d0751133ba63d9ea4ce180ad
Peter Inglesby <peter.inglesby@gmail.com>
2016-01-13 10:24:15 +0000

    Use version of PyInstaller with even more verbose logging

--- package/install_osx.sh
+++ package/install_osx.sh
@@ -2,7 +2,7 @@
 set -ev
 brew update >/dev/null 2>&1  # This produces a lot of output that's not very interesting
 brew install python3
-sudo pip install git+git://github.com/inglesp/pyinstaller.git@6390b58dd2c4bb0f96642f811b6946a197b1c35a
+sudo pip install git+git://github.com/inglesp/pyinstaller.git@e299fcb1d630c65e91019f6e6bff6fa3b87e8bc1
 brew install pyqt5 --with-python3
 python3 -c 'import PyQt5; print(PyQt5.__file__)'
 python3 -c 'import PyQt5.QtWidgets; print(PyQt5.QtWidgets.__file__)'
