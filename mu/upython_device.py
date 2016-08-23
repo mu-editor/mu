d77e6414ac5a2c98de45a0c09772ae1242921600
Victor Rajewski <askvictor@gmail.com>
2016-08-24 08:28:10 +1000

    First attempt at implementing execute_commands, and other functions in base class

--- mu/upython_device.py
+++ mu/upython_device.py
@@ -1,4 +1,7 @@
 import logging
+import time
+import ast
+import os
 from PyQt5 import QtSerialPort, QtWebSockets
 from PyQt5.QtCore import QIODevice, QUrl, QTimer
 from mu import config
@@ -10,6 +13,7 @@ class uPythonDevice():
     """Abstract base class/Interface for defining a micropython device."""
     def __init__(self, data_received_callback=None):
         self.data_received_callback = data_received_callback
+        self.saved_callback = None
         pass
 
     def __del__(self):
@@ -24,6 +28,16 @@ class uPythonDevice():
     def remove_callback(self):
         self.data_received_callback = None
 
+    def suspend_callback(self):
+        if self.data_received_callback is not None:
+            self.saved_callback = self.data_received_callback
+        self.remove_callback()
+
+    def reinstate_callback(self):
+        if self.saved_callback is not None:
+            self.add_callback(self.saved_callback)
+        self.saved_callback = None
+
     def send(self, bs):
         """send str to the device"""
         pass
@@ -34,20 +48,152 @@ class uPythonDevice():
             self.data_received_callback(data)
 
     def execute_commands(self, commands):
-        """executes the commands in the list `commands` on the device via the REPL"""
+        """
+        executes the commands in the list `commands` on the device via the REPL
+
+        For this to work correctly, a particular sequence of commands needs to be
+        sent to put the device into a good state to process the incoming command.
+
+        Returns the stdout and stderr output from the micro:bit.
+        """
+        result = b''
+        self.raw_on()
+        # Write the actual command and send CTRL-D to evaluate.
+        for command in commands:
+            command_bytes = command.encode('utf-8')
+            for i in range(0, len(command_bytes), 32):
+                self.send(command_bytes[i:min(i + 32, len(command_bytes))])
+                time.sleep(0.01)
+            self.send(b'\x04')
+            response = bytearray()
+            while not response.endswith(b'\x04>'):  # Read until prompt.
+                response.extend(self.read_all())
+            out, err = response[2:-2].split(b'\x04', 1)  # Split stdout, stderr
+            result += out
+            if err:
+                return b'', err
+        self.raw_off()
+        return result, err
+
         pass
+
+    def raw_on(self):
+        """ Puts the device into raw mode. """
+        self.send(b'\x03')  # Send CTRL-C to break out of loop.
+        self.read_until(b'\n>')  # Flush buffer until prompt.
+        self.send(b'\x01')  # Go into raw mode.
+        self.read_until(b'\r\n>OK')  # Flush buffer until raw mode prompt.
+
+    def raw_off(self):
+        """ Takes the device out of raw mode. """
+        self.send(b'\x02')  # Send CTRL-B to get out of raw mode.
+
+    def read(self, count=1):
+        """Returns `count` bytes from buffer"""
+        pass
+
+    def read_all(self):
+        """Returns all bytes in buffer"""
+        pass
+
+    def read_until(self, terminator=b'\n', size=None):
+        """
+        Read until a termination sequence is found ('\n' by default), the size
+        is exceeded or until timeout occurs.
+        """
+        lenterm = len(terminator)
+        line = bytearray()
+        while True:
+            c = self.read(1)
+            if c:
+                line += c
+                if line[-lenterm:] == terminator:
+                    break
+                if size is not None and len(line) >= size:
+                    break
+            else:
+                break
+        return bytes(line)
 
     def list_files(self):
-        pass
+        """
+        Returns a list of the files on the connected device or raises an IOError if
+        there's a problem.
+        """
+        out, err = self.execute_commands([
+            'import os',
+            'print(os.listdir())',
+        ])
+        if err:
+            raise IOError(err)
+        return ast.literal_eval(out.decode('utf-8'))
 
-    def put_file(self, local_path, remote_filename):
-        pass
+    def put_file(self, local_path, remote_filename=None):
+        """
+        Puts a referenced file on the LOCAL file system onto the
+        file system on the remote device.
+
+        Returns True for success or raises an IOError if there's a problem.
+        """
+        if not os.path.isfile(local_path):
+            raise IOError('No such file.')
+        with open(local_path, 'rb') as local:
+            content = local.read()
+        if remote_filename is None:
+            remote_filename = os.path.basename(local_path)
+        commands = [
+            "fd = open('{}', 'wb')".format(remote_filename),
+            "f = fd.write",
+        ]
+        while content:
+            line = content[:64]
+            commands.append('f(' + repr(line) + ')')
+            content = content[64:]
+        commands.append('fd.close()')
+        out, err = self.execute(commands)
+        if err:
+            raise IOError(err)
+        return True
 
     def get_file(self, remote_filename, local_path=None):
-        pass
+        """
+        Gets a referenced file on the device's file system and copies it to the
+        target (or current working directory if unspecified).
+
+        Returns True for success or raises an IOError if there's a problem.
+        """
+        if local_path is None:
+            local_path = remote_filename
+        commands = [  # TODO - should ensure ESP OS debugging is off
+            "f = open('{}', 'rb')".format(remote_filename),
+            "r = f.read",
+            "result = True",
+            "while result:\n    result = r(32)\n    if result:\n        print(result, end='')\n",
+            #"while f.read(32): print(_, end='')\n",
+            "f.close()",
+        ]
+        out, err = self.execute(commands)
+        if err:
+            raise IOError(err)
+        # Recombine the bytes while removing "b'" from start and "'" from end.
+        with open(local_path, 'wb') as f:
+            f.write(out)
+        return True
 
     def del_file(self, remote_filename):
-        pass
+        """
+        Removes a referenced file on the micro:bit.
+
+        Returns True for success or raises an IOError if there's a problem.
+        """
+        commands = [
+            "import os",
+            "os.remove('{}')".format(remote_filename),
+        ]
+        out, err = self.execute(commands)
+        if err:
+            raise IOError(err)
+        return True
 
 
 class WEBREPLuPythonDevice(uPythonDevice):
@@ -71,6 +217,18 @@ class WEBREPLuPythonDevice(uPythonDevice):
     def data_received(self):
         super().data_received(self.buffer)
         self.buffer = ''
+
+    def read(self, count):
+        self.suspend_callback()
+        data = "" #TODO
+        self.reinstate_callback()
+        return data
+
+    def read_all(self):
+        self.suspend_callback()
+        data = "" #TODO
+        self.reinstate_callback()
+        return data
 
     def send(self, bs):
         self.ws.sendTextMessage(bs.decode('utf-8'))  # sendTextMessage takes a string
@@ -101,7 +259,19 @@ class SerialuPythonDevice(uPythonDevice):
         return self.serial.write(bs)  # serial.write takes a byte array
 
     def data_available(self):
-        self.data_received(self.serial.readAll())
+        self.data_received(self.read_all())
+
+    def read(self, count):
+        self.suspend_callback()
+        data = self.serial.read(count)
+        self.reinstate_callback()
+        return data
+
+    def read_all(self):
+        self.suspend_callback()
+        data = self.serial.readAll()
+        self.reinstate_callback()
+        return data
 
 
 class MicrobitDevice(SerialuPythonDevice):
