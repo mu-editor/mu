f9cc20c775c942aba3674282284db25893c5a42e
Victor Rajewski <askvictor@gmail.com>
2016-10-04 17:31:50 +1100

    deal with file overwrite in filesystem pans

--- mu/interface.py
+++ mu/interface.py
@@ -892,6 +892,20 @@ class MuFileList(QListWidget):
         self.setAcceptDrops(True)
         sibling.setAcceptDrops(True)
 
+    def show_confirm_overwrite_dialog(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+
+        msg.setText("File already exists; overwrite it?")
+        msg.setWindowTitle("File already exists")
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+
+        retval = msg.exec_()
+        if retval == QMessageBox.Ok:
+            return True
+        else:
+            return False
+
 
 class MicrobitFileList(MuFileList):
     """
@@ -907,15 +921,18 @@ class MicrobitFileList(MuFileList):
         source = event.source()
         self.disable(source)
         if isinstance(source, LocalFileList):
-            local_filename = os.path.join(self.home,
-                                          source.currentItem().text())
-            logger.info("Putting {}".format(local_filename))
-            try:
-                self.parent().device.put_file(local_filename)
-                super().dropEvent(event)
-            except Exception as ex:
-                logger.error(ex)
+            if self.findItems(source.currentItem().text(), Qt.MatchExactly) \
+                    and self.show_confirm_overwrite_dialog():
+                local_filename = os.path.join(self.home,
+                                              source.currentItem().text())
+                logger.info("Putting {}".format(local_filename))
+                try:
+                    self.parent().device.put_file(local_filename)
+                    super().dropEvent(event)
+                except Exception as ex:
+                    logger.error(ex)
         self.enable(source)
+        self.parent().ls()
 
     def contextMenuEvent(self, event):
         menu = QMenu(self)
@@ -949,17 +966,20 @@ class LocalFileList(MuFileList):
         source = event.source()
         self.disable(source)
         if isinstance(source, MicrobitFileList):
-            microbit_filename = source.currentItem().text()
-            local_filename = os.path.join(self.home,
-                                          microbit_filename)
-            logger.debug("Getting {} to {}".format(microbit_filename,
-                                                   local_filename))
-            try:
-                self.parent().device.get_file(microbit_filename, local_filename)
-                super().dropEvent(event)
-            except Exception as ex:
-                logger.error(ex)
+            if self.findItems(source.currentItem().text(), Qt.MatchExactly) \
+                    and self.show_confirm_overwrite_dialog():
+                microbit_filename = source.currentItem().text()
+                local_filename = os.path.join(self.home,
+                                              microbit_filename)
+                logger.debug("Getting {} to {}".format(microbit_filename,
+                                                       local_filename))
+                try:
+                    self.parent().device.get_file(microbit_filename, local_filename)
+                    super().dropEvent(event)
+                except Exception as ex:
+                    logger.error(ex)
         self.enable(source)
+        self.parent().ls()
 
 
 class FileSystemPane(QFrame):
