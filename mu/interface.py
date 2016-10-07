310595626db0b67d6a5fc3668ba440fc3a0cf452
Victor Rajewski <askvictor@gmail.com>
2016-10-07 17:32:33 +1100

    control-keys should now work in REPL on Windows and Linux (not tested though)

--- mu/interface.py
+++ mu/interface.py
@@ -780,8 +780,9 @@ class REPLPane(QTextEdit):
             msg = b'\x1B[C'
         elif key == Qt.Key_Left:
             msg = b'\x1B[D'
-        elif platform.system() == 'Darwin' and \
-                data.modifiers() == Qt.MetaModifier:
+        elif data.modifiers() == Qt.ControlModifier or \
+                (platform.system() == 'Darwin' and
+                    data.modifiers() == Qt.MetaModifier):
             # Handle the Control key. On OSX/macOS/Darwin (python calls this
             # platform Darwin), this is handled by Qt.MetaModifier. Other
             # platforms (Linux, Windows) call this Qt.ControlModifier. Go
