2f12afe78e19bab6d4dc6bf6f486e60dda989722
Tim Golden <mail@timgolden.me.uk>
2016-12-21 17:09:02 +0000

    Refactor so pyflakes & pycodestyle are now present

--- check.py
+++ check.py
@@ -1,4 +1,5 @@
 import os, sys
+import fnmatch
 import inspect
 import subprocess
 import textwrap
@@ -12,11 +13,53 @@ def export(function):
     _exported[function.__name__] = function
     return function
 
+def walk(start_from=".", include_patterns=None, exclude_patterns=None):
+    _include_patterns = include_patterns or set(["*"])
+    _exclude_patterns = exclude_patterns or set()
+
+    for dirpath, dirnames, filenames in os.walk("mu"):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            if not any(fnmatch.fnmatch(filepath, pattern) for pattern in include_patterns):
+                continue
+            if any (fnmatch.fnmatch(filepath, pattern) for pattern in exclude_patterns):
+                continue
+            yield filepath
+
+INCLUDE_PATTERNS = {
+    "*.py"
+}
+EXCLUDE_PATTERNS = {
+    r"build\*",
+    r"docs\*",
+    r"mu\contrib\*",
+    r"mu\resources\api.py",
+}
+
 @export
 def test(*args):
-    """Call py.test to run the test suite
+    """Call py.test to run the test suite with additional args
     """
-    return subprocess.call(["py.test"] + list(args))
+    return subprocess.call(["py.test.exe"] + list(args))
+
+def _check(executable, *args):
+    for filepath in walk("mu", INCLUDE_PATTERNS, EXCLUDE_PATTERNS):
+        subprocess.call([executable, filepath] + list(args))
+    for filepath in walk("tests", INCLUDE_PATTERNS, EXCLUDE_PATTERNS):
+        subprocess.call([executable, filepath] + list(args))
+
+@export
+def pyflakes(*args):
+    """Call pyflakes on all .py files outside the docs and contrib directories
+    """
+    return _check("pyflakes.exe", *args)
+
+@export
+def pycodestyle(*args):
+    """Call pyflakes on all .py files outside the docs and contrib directories
+    """
+    args = ("--ignore=E731,E402",) + args
+    return _check("pycodestyle.exe", *args)
 
 @export
 def help():
