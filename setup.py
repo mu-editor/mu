b6596f02a2476557933899f4caa4d3a579921186
pydo <964683112@qq.com>
2018-03-02 12:22:47 +0800

    up pyzero dependency to the latest version 1.2
    up pyzero dependency to the latest version 1.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=['pycodestyle==2.3.1', 'pyflakes==1.6.0',
                       'pyserial==3.4', 'pyqt5==5.10', 'qscintilla==2.10.2',
                       'qtconsole==4.3.1', 'matplotlib==2.1.2',
-                      'pgzero==1.1', 'PyQtChart==5.10'],
+                      'pgzero==1.2', 'PyQtChart==5.10'],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
