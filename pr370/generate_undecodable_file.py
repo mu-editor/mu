b6b25d43dde2584d359ad0d4d9cd7e832ed42f95
Tim Golden <mail@timgolden.me.uk>
2018-03-06 18:41:50 +0000

    Add an encoding cookie

--- pr370/generate_undecodable_file.py
+++ pr370/generate_undecodable_file.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- #
 #
 # This will generate a file containing a cp1252-encoded pound sign
 # which won't decide in utf-8 because the same byte sequence
@@ -5,6 +6,6 @@
 #
 f = open("t.py", "w", encoding="cp1252")
 f.write("# -*- coding: utf-8 -*-\n")
-f.write("£")
+f.write("Â£")
 f.close()
 
