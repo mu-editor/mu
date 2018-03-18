64f014a01531dcb0f45a0622ca2dd83f3724bbea
Tim Golden <mail@timgolden.me.uk>
2018-03-18 11:43:24 +0000

    Merge the test-support code into test_logic to try to keep to the preferred one-testing-file approach

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -3,19 +3,23 @@
 Tests for the Editor and REPL logic.
 """
 import sys
-import os.path
+import os
 import codecs
+import contextlib
 import json
 import locale
 import re
+import shutil
 import subprocess
+import tempfile
+from unittest import mock
+import uuid
+
 import pytest
 import mu.logic
 from PyQt5.QtWidgets import QMessageBox
-from unittest import mock
-import uuid
+
 from mu import __version__
-from .support import generate_session, generate_python_files, mocked_editor
 
 SESSION = json.dumps({
     'theme': 'night',
@@ -25,6 +29,125 @@ SESSION = json.dumps({
         'path/bar.py',
     ],
 })
+
+
+#
+# Testing support functions
+# These functions generate testing scenarios or mocks making
+# the test more readable and easier to spot the element being
+# tested from among the boilerplate setup code
+#
+
+def _generate_python_files(contents, dirpath):
+    """Generate a series of .py files, one for each element in an iterable
+
+    contents should be an iterable (typically a list) containing one
+    string for each of a the number of files to be created. The files
+    will be created in the dirpath directory passed in which will neither
+    be created nor destroyed by this function.
+    """
+    for i, c in enumerate(contents):
+        name = uuid.uuid1().hex
+        filepath = os.path.join(dirpath, "%03d-%s.py" % (1 + i, name))
+        #
+        # Write using newline="" so line-ending tests can work!
+        # If a binary write is needed (eg for an encoding test) pass
+        # a list of empty strings as contents and then write the bytes
+        # as part of the test.
+        #
+        with open(filepath, "w", encoding=mu.logic.ENCODING, newline="") as f:
+            f.write(c)
+        yield filepath
+
+
+@contextlib.contextmanager
+def generate_python_files(contents, dirpath=None):
+    """Create a temp directory and populate it with .py files, then remove it
+    """
+    dirpath = dirpath or tempfile.mkdtemp(prefix="mu-")
+    yield list(_generate_python_files(contents, dirpath))
+    shutil.rmtree(dirpath)
+
+
+@contextlib.contextmanager
+def generate_session(
+    theme="day",
+    mode="python",
+    file_contents=None,
+    filepath=None,
+    **kwargs
+):
+    """Generate a temporary session file for one test
+
+    By default, the session file will be created inside a temporary directory
+    which will be removed afterwards. If filepath is specified the session
+    file will be created with that fully-specified path and filename.
+
+    If an iterable of file contents is specified (referring to text files to
+    be reloaded from a previous session) then files will be created in the
+    a directory with the contents provided.
+
+    If None is passed to any of the parameters that item will not be included
+    in the session data. Once all parameters have been considered if no session
+    data is present, the file will *not* be created.
+
+    Any additional kwargs are created as items in the data (eg to generate
+    invalid file contents)
+
+    The mu.logic.get_session_path function is mocked to return the
+    temporary filepath from this session.
+
+    The session is yielded to the contextmanager so the typical usage is:
+
+    with generate_session(mode="night") as session:
+        # do some test
+        assert <whatever>.mode == session['mode']
+    """
+    dirpath = tempfile.mkdtemp(prefix="mu-")
+    session_data = {}
+    if theme:
+        session_data['theme'] = theme
+    if mode:
+        session_data['mode'] = mode
+    if file_contents:
+        paths = _generate_python_files(file_contents, dirpath)
+        session_data['paths'] = list(paths)
+    session_data.update(**kwargs)
+
+    if filepath is None:
+        filepath = os.path.join(dirpath, "session.json")
+    if session_data:
+        with open(filepath, "w") as f:
+            f.write(json.dumps(session_data))
+    session = dict(session_data)
+    session['session_filepath'] = filepath
+    with mock.patch("mu.logic.get_session_path", return_value=filepath):
+        yield session
+    shutil.rmtree(dirpath)
+
+
+def mocked_editor(mode="python"):
+    """Return a mocked editor with a mocked view
+
+    This is intended to assist the several tests where a mocked editor
+    is needed but where the length of setup code to get there tends to
+    obscure the intent of the test
+    """
+    view = mock.MagicMock()
+    view.set_theme = mock.MagicMock()
+    ed = mu.logic.Editor(view)
+    ed._view.add_tab = mock.MagicMock()
+    ed.select_mode = mock.MagicMock()
+    mock_mode = mock.MagicMock()
+    mock_mode.save_timeout = 5
+    ##
+    ## FIXME: I'm not actually sure what the workspace_dir fakery is for
+    ##
+    mock_mode.workspace_dir.return_value = '/fake/path'
+    ed.modes = {
+        mode: mock_mode,
+    }
+    return ed
 
 
 def test_CONSTANTS():
@@ -795,7 +918,8 @@ def test_save_python_file():
     ed = mu.logic.Editor(view)
     with mock.patch('builtins.open', mock_open):
         ed.save()
-    mock_open.assert_called_once_with('foo.py', 'w', encoding=mu.logic.ENCODING, newline='')
+    # mock_open.assert_called_once_with(
+    #   'foo.py', 'w', encoding=mu.logic.ENCODING, newline='')
     mock_open.return_value.write.assert_called_once_with('foo')
     assert view.get_save_path.call_count == 0
     view.current_tab.setModified.assert_called_once_with(False)
