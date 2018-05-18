402f6642f25a765c658f11cffd38f4cea4563521
Nicholas H.Tollervey <ntoll@ntoll.org>
2018-05-18 20:15:48 +0100

    Another attempt to fix the DistUtilsOptionError.

--- package/install_osx.sh
+++ package/install_osx.sh
@@ -4,8 +4,3 @@ brew update >/dev/null 2>&1  # This produces a lot of output that's not very int
 
 # Install Python 3.6
 brew install python
-
-# Create and enable a virtualenv
-pip3 install virtualenv
-virtualenv --python=/usr/local/bin/python3 venv
-source venv/bin/activate
