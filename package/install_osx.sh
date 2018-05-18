71e02135a49a0334f8b2a9451be57b913b3985ae
Nicholas H.Tollervey <ntoll@ntoll.org>
2018-05-18 15:54:06 +0100

    Try building again with a virtualenv to avoid brew/pip related fubar.

--- package/install_osx.sh
+++ package/install_osx.sh
@@ -4,3 +4,7 @@ brew update >/dev/null 2>&1  # This produces a lot of output that's not very int
 
 # Install Python 3.6
 brew install python
+
+# Create and enable a virtualenv
+virtualenv --python=/usr/local/bin/python3 venv
+source venv/bin/activate
