148d286b4ffba3dcfc0a29a9b961c8d9b9f27f47
Nicholas H.Tollervey <ntoll@ntoll.org>
2018-07-01 22:42:00 +0100

    Use pyenv to install Python 3.6.5.

--- package/install_osx.sh
+++ package/install_osx.sh
@@ -3,7 +3,9 @@ set -ev
 brew update >/dev/null 2>&1  # This produces a lot of output that's not very interesting
 
 # Install Python 3.6
-brew install python
+brew install pyenv
+pyenv install 3.6.5
+pyenv local 3.6.5
 # The following are needed for Matplotlib
 brew install freetype
 brew install pkg-config
