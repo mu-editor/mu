0b29b41021f7bdc4d101a80f7da4a97e945fef07
Nicholas H.Tollervey <ntoll@ntoll.org>
2018-07-01 19:53:38 +0100

    Add dependencies for Matplotlib in OSX

--- package/install_osx.sh
+++ package/install_osx.sh
@@ -4,3 +4,6 @@ brew update >/dev/null 2>&1  # This produces a lot of output that's not very int
 
 # Install Python 3.6
 brew install python
+# The following are needed for Matplotlib
+brew install freetype
+brew install pkg-config
