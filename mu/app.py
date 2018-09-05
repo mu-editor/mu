2ace50da26a0bd557af0b73de5772d022b1cebe9
Murilo Polese <murilopolese@gmail.com>
2018-09-05 23:22:00 +0100

    Include mode on app

--- mu/app.py
+++ mu/app.py
@@ -34,7 +34,7 @@ from mu.logic import Editor, LOG_FILE, LOG_DIR, DEBUGGER_PORT, ENCODING
 from mu.interface import Window
 from mu.resources import load_pixmap, load_icon
 from mu.modes import (PythonMode, AdafruitMode, MicrobitMode, DebugMode,
-                      PyGameZeroMode)
+                      PyGameZeroMode, PixelKitMode)
 from mu.debugger.runner import run as run_debugger
 from mu.interface.themes import NIGHT_STYLE, DAY_STYLE, CONTRAST_STYLE
 
@@ -78,6 +78,7 @@ def setup_modes(editor, view):
         'adafruit': AdafruitMode(editor, view),
         'microbit': MicrobitMode(editor, view),
         'debugger': DebugMode(editor, view),
+        'pixelkit': PixelKitMode(editor, view),
     }
 
     # Check if pgzero is available (without importing it)
