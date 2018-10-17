6bb6838a803ecd61468a3f056a4404ca2b7a12d9
Tiago Montes <tiago.montes@gmail.com>
2018-10-17 12:18:59 +0100

    Moved localedetect module tests to their own module.

--- tests/test_init.py
+++ tests/test_init.py
@@ -3,12 +3,9 @@
 Tests for the module __init__ file.
 """
 import os
-import subprocess
 import sys
 import importlib
 from unittest import mock
-
-import pytest
 
 import mu
 
@@ -33,109 +30,6 @@ def test_gettext_translation():
         del os.environ['LC_ALL']
     assert translation.call_count == 1
     assert translation.call_args[1]['languages'] == ['es_ES']
-
-
-@pytest.mark.parametrize('value, exc', [
-    (None, TypeError('type error text')),
-    (None, ValueError('value error text')),
-    (('', ''), None),
-])
-def test_localedetect_getdefaultlocale_failure_calls_fail_handler(value, exc):
-    """
-    Test that either a Type/ValueError exception in the locale.getdefaultlocale
-    call, or an empty language_code, are detected and that the fail_handler is
-    called.
-    """
-    mock_locale = mock.MagicMock(return_value=value, side_effect=exc)
-    mock_fail_handler = mock.Mock()
-
-    with mock.patch('locale.getdefaultlocale', mock_locale):
-        mu.localedetect.language_code(fail_handler=mock_fail_handler)
-
-    assert mock_fail_handler.call_count == 1
-
-
-@pytest.mark.parametrize('fallback, value, exc, expected', [
-    ('fallback', 'a language code', None, 'a language code'),
-    ('fallback', '', None, 'fallback'),
-    ('fallback', None, Exception('fail handler failed!'), 'fallback'),
-])
-def test_localedetect_fail_handler_handling(fallback, value, exc, expected):
-    """
-    Test that when localedetect.language_code uses the fail_handler, it
-    returns its returned value; unless it's empty or it raises an exception: in
-    that case, the passed in fallback value should be returned.
-    """
-    # Force fail_handler to be used
-    mock_locale = mock.MagicMock(return_value=('', ''))
-    mock_fail_handler = mock.Mock(return_value=value, side_effect=exc)
-
-    with mock.patch('locale.getdefaultlocale', mock_locale):
-        lc = mu.localedetect.language_code(fallback=fallback,
-                                           fail_handler=mock_fail_handler)
-
-    assert lc == expected
-
-
-def test_localedetect_default_fail_handler_unsupported_platform(caplog):
-    """
-    Test that localedetect.language_code uses a platform dependent fail
-    handler, by default, that, in unsupported platforms, will log a warning
-    message, returning nothing. With that, localedetect.language_code should
-    return the passed in fallback value.
-    """
-    # Force fail_handler to be used
-    mock_locale = mock.MagicMock(return_value=('', ''))
-
-    with mock.patch('locale.getdefaultlocale', mock_locale), \
-            mock.patch('sys.platform', 'zx81'):
-        lc = mu.localedetect.language_code(fallback='sinclair')
-
-    # Assert message was logged: no platform detection exists for 'zx81'.
-    expected_log_msg = "No platform specific language detection for 'zx81'."
-    for record in caplog.records:
-        if record.getMessage() == expected_log_msg:
-            break
-    else:
-        assert False, 'Expected log message missing.'
-
-    # Assert fallback value was returned.
-    assert lc == 'sinclair'
-
-
-def test_localedetect_default_fail_handler_supported_platform():
-    """
-    Test that localedetect.language_code uses a platform dependent fail
-    handler, by default, that, on supported platforms is named
-    '_language_code_{sys.platform}.
-    """
-    # Force fail_handler to be used
-    mock_locale = mock.MagicMock(return_value=('', ''))
-    mock_zx81 = mock.Mock(return_value='sinclair')
-
-    try:
-        mu.localedetect._language_code_zx81 = mock_zx81
-        with mock.patch('locale.getdefaultlocale', mock_locale), \
-                mock.patch('sys.platform', 'zx81'):
-            lc = mu.localedetect.language_code()
-    finally:
-        del mu.localedetect._language_code_zx81
-
-    assert mock_zx81.call_count == 1
-    assert lc == 'sinclair'
-
-
-@pytest.mark.skipif(sys.platform != 'darwin',
-                    reason='Platform specific: macOS.')
-def test_localedetect_language_code_macos():
-    """
-    Tests that localedetect._language_code_darwin returns the same string as
-    the command line: defaults read -g AppleLocale.
-    """
-    mu_lc = mu.localedetect._language_code_darwin()
-    cmd_line_lc = subprocess.getoutput('defaults read -g AppleLocale')
-
-    assert mu_lc == cmd_line_lc
 
 
 def test_defaultlocale_type_error():
---
+++ tests/test_localedetect.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for the localedetect module.
+"""
+import subprocess
+import sys
+from unittest import mock
+
+import pytest
+
+from mu import localedetect
+
+
+@pytest.mark.parametrize('value, exc', [
+    (None, TypeError('type error text')),
+    (None, ValueError('value error text')),
+    (('', ''), None),
+])
+def test_language_code_getdefaultlocale_failure_calls_fail_handler(value, exc):
+    """
+    If the locale.getdefaultlocale() call used by the language_code() function
+    either a) raises a Type/ValueError or b) returns an empty language code,
+    the passed in fail_handler is called.
+    """
+    mock_getdefaultlocale = mock.MagicMock(return_value=value, side_effect=exc)
+    mock_fail_handler = mock.Mock()
+
+    with mock.patch('locale.getdefaultlocale', mock_getdefaultlocale):
+        localedetect.language_code(fail_handler=mock_fail_handler)
+
+    assert mock_fail_handler.call_count == 1
+
+
+@pytest.mark.parametrize('fallback, value, exc, expected', [
+    ('fallback_lang', 'nice_lang', None, 'nice_lang'),
+    ('fallback_lang', '', None, 'fallback_lang'),
+    ('fallback_lang', None, Exception('Fail handler failed'), 'fallback_lang'),
+])
+def test_language_code_fail_handler_handling(fallback, value, exc, expected):
+    """
+    When language_code() uses the fail_handler, it returns either whatever
+    the fail_handler returns, unless that raises an exception or returns an
+    empty/false-y value: it such cases, language_code() returns the passed in
+    fallback value.
+    """
+    # Force fail_handler to be used
+    mock_getdefaultlocale = mock.MagicMock(return_value=('', ''))
+    mock_fail_handler = mock.Mock(return_value=value, side_effect=exc)
+
+    with mock.patch('locale.getdefaultlocale', mock_getdefaultlocale):
+        lang_code = localedetect.language_code(fallback=fallback,
+                                               fail_handler=mock_fail_handler)
+
+    assert lang_code == expected
+
+
+def test_language_code_default_fail_handler_unsupported_platform(caplog):
+    """
+    The language_code() function uses a platform dependent fail handler, by
+    default, that, on unsupported platforms, will log a warning message,
+    returning nothing; in such cases, language_code() returns the passed in
+    fallback value.
+    """
+    # Force fail_handler to be used
+    mock_getdefaultlocale = mock.MagicMock(return_value=('', ''))
+
+    # Fake 'zx81' platform which is (unfortunatelly?) an unsupported platform.
+    with mock.patch('locale.getdefaultlocale', mock_getdefaultlocale), \
+            mock.patch('sys.platform', 'zx81'):
+        lang_code = localedetect.language_code(fallback='sinclair')
+
+    # Assert message was logged: no platform detection exists for 'zx81'.
+    expected_log_msg = "No platform specific language detection for 'zx81'."
+    for record in caplog.records:
+        if record.getMessage() == expected_log_msg:
+            break
+    else:
+        pytest.fail('Expected log message missing.')
+
+    # Assert fallback value was returned.
+    assert lang_code == 'sinclair'
+
+
+def test_language_code_default_fail_handler_supported_platform():
+    """
+    The language_code() function uses a platform dependent fail handler that,
+    by default, on supported platforms, is named _language_code_{sys.platform}.
+    """
+    # Force fail_handler to be used
+    mock_getdefaultlocale = mock.MagicMock(return_value=('', ''))
+    mock_language_code_zx81 = mock.Mock(return_value='sinclair')
+
+    try:
+        localedetect._language_code_zx81 = mock_language_code_zx81
+        with mock.patch('locale.getdefaultlocale', mock_getdefaultlocale), \
+                mock.patch('sys.platform', 'zx81'):
+            lang_code = localedetect.language_code()
+    finally:
+        del localedetect._language_code_zx81
+
+    assert mock_language_code_zx81.call_count == 1
+    assert lang_code == 'sinclair'
+
+
+@pytest.mark.skipif(sys.platform != 'darwin',
+                    reason='Platform specific test for macOS.')
+def test_language_code_macos():
+    """
+    The _language_code_darwin() function returns the same string as the macOS
+    command line "defaults read -g AppleLocale".
+    """
+    mu_lang_code = localedetect._language_code_darwin()
+    cmd_line_lang_code = subprocess.getoutput('defaults read -g AppleLocale')
+
+    assert mu_lang_code == cmd_line_lang_code
