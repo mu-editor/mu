1b4829c81e646ea77deef5076a2800d7e8f9008d
Tiago Montes <tiago.montes@gmail.com>
2019-02-16 18:36:55 +0000

    Removed useless blank to force GitHub checks to run again.

--- mu/__init__.py
+++ mu/__init__.py
@@ -3,7 +3,6 @@ import os
 
 from PyQt5.Qt import QLocale
 
-
 # Configure locale and language
 # Define where the translation assets are to be found.
 localedir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'locale'))
