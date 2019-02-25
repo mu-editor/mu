0e91b408d12fe257f821f91afee2c43a1780a6d0
Tiago Montes <tiago.montes@gmail.com>
2019-02-25 17:42:14 +0000

    Windows packaging revamp: setup.py is source of dependecy truth.

--- win_installer.py
+++ win_installer.py
@@ -23,15 +23,52 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import sys
-import requests
-import zipfile
+
+import os
+import platform
+import shutil
 import subprocess
+import sys
+import tempfile
+import textwrap
+import zipfile
+
+import requests
+
+
+PYNSIST_CFG_TEMPLATE = """
+[Application]
+name=Mu
+version={version}
+entry_point=mu.app:run
+icon={icon_file}
+publisher=Nicholas H.Tollervey
+license_file={license_file}
+
+[Command mu-debug]
+entry_point=mu.app:debug
+
+[Command pgzrun]
+entry_point=pgzero.runner:main
+
+[Python]
+version=3.6.3
+bitness={bitness}
+format=bundled
+
+[Include]
+pypi_wheels= 
+{pypi_wheels}
+
+files=lib
+"""
 
 
 URL = 'https://github.com/mu-editor/mu_tkinter/releases/download/'
-ASSETS_32 = URL + '0.3/pynsist_tkinter_3.6_32bit.zip'
-ASSETS_64 = URL + '0.3/pynsist_tkinter_3.6_64bit.zip'
+ASSETS = {
+    '32': URL + '0.3/pynsist_tkinter_3.6_32bit.zip',
+    '64': URL + '0.3/pynsist_tkinter_3.6_64bit.zip',
+}
 
 
 def download_file(url):
@@ -55,32 +92,102 @@ def unzip_file(filename):
         z.extractall()
 
 
-def run(bitness):
+def frozen_requirements(repo_root, encoding, venv_name='mu-venv'):
     """
+    Returns the "pip freeze" output as a string after setting up
+    a temporary virtual environment where the Mu is installed.
+    """
+    print(f'Creating {venv_name} virtual environment...')
+    subprocess.run([sys.executable, '-m', 'venv', venv_name])
+    print(f'Installing mu into {venv_name}...')
+    venv_python = os.path.join(venv_name, 'Scripts', 'python.exe')
+    subprocess.run([venv_python, '-m', 'pip', 'install', repo_root])
+    print('Getting frozen requirements...')
+    result = subprocess.check_output([venv_python, '-m', 'pip', 'freeze'])
+    result = result.decode(encoding)
+    print(f'Removing {venv_name} virtual environment...')
+    shutil.rmtree(venv_name, ignore_errors=True)
+    return result
+
+
+def platform_bitness():
+    """
+    Returns the Python executable bitness, as a string: '32' or '64'.
+    Raises a RuntimeError if the platform module reports differently.
+    """
+    bitness, _ = platform.architecture()
+    result = bitness[:2]
+    if result not in ('32', '64'):
+        raise RuntimeError(f'Unsupported platform.architecture: {bitness}')
+    print('Python bitness:', result)
+    return result
+
+
+def mu_version(repo_root):
+    """
+    Returns the Mu version string as declared in mu/__about__.py.
+    """
+    about = {}
+    with open(os.path.join(repo_root, 'mu', '__about__.py')) as f:
+        exec(f.read(), about)
+    return about['__version__']
+
+
+def create_pynsist_cfg_file(encoding, **kw):
+    """
+    TODO: WRITE ME!
+    """
+    filename = 'pynsist.cfg'
+    pynsist_cfg_payload = PYNSIST_CFG_TEMPLATE.format(**kw)
+    with open(filename, 'wt', encoding=encoding) as f:
+        f.write(pynsist_cfg_payload)
+    print('Created pynsist.cfg file:')
+    print(pynsist_cfg_payload)
+    return filename
+
+
+def run(repo_root):
+    """
+    TODO: REVIEW THIS!
     Given a certain bitness, coordinate the downloading and unzipping of the
     appropriate assets.
     """
-    print('Downloading tkinter for {}bit platform.'.format(bitness))
-    if bitness == 32:
-        filename = download_file(ASSETS_32)
-    else:
-        filename = download_file(ASSETS_64)
-    print('Unzipping {}.'.format(filename))
-    unzip_file(filename)
-    print('Running pynsist')
-    subprocess.call(['pynsist', 'win_installer{}.cfg'.format(bitness)])
+    starting_cwd = os.getcwd()
+    with tempfile.TemporaryDirectory(suffix='.mu-pynsist') as work_dir:
+        print('Working directory:', work_dir)
+        os.chdir(work_dir)
+
+        icon_file = os.path.join(repo_root, 'package', 'icons', 'win_icon.ico')
+        license_file = os.path.join(repo_root, 'LICENSE')
+        bitness = platform_bitness()
+        requirements = frozen_requirements(repo_root, encoding='latin1')
+
+        pynsist_cfg_filename = create_pynsist_cfg_file(
+            encoding='latin1',
+            version=mu_version(repo_root),
+            icon_file=icon_file,
+            license_file=license_file,
+            bitness=bitness,
+            pypi_wheels=textwrap.indent(requirements, ' '*4),
+        )
+
+        print('Downloading tkinter for {}bit platform.'.format(bitness))
+        filename = download_file(ASSETS[bitness])
+
+        print('Unzipping {}.'.format(filename))
+        unzip_file(filename)
+
+        print('Running pynsist')
+        subprocess.call(['pynsist', pynsist_cfg_filename])
+
+        print('DONE!!!')
+        import time; time.sleep(60)
+
+        os.chdir(starting_cwd)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        try:
-            bitness = int(sys.argv[1])
-            if bitness not in (32, 64):
-                raise ValueError
-            run(bitness)
-        except ValueError:
-            print('Accepted bitness: 32 or 64')
-    else:
-        print('Download the tkinter assets needed to create a Windows '
-              'installer. Supply either 32 or 64 as an argument to indicate '
-              'bitness.')
+    if len(sys.argv) != 2 or not sys.argv[1].endswith('setup.py'):
+        sys.exit('Supply the setup.py file as an argument, please.')
+    repo_root = os.path.abspath(os.path.dirname(sys.argv[1]))
+    run(repo_root)
