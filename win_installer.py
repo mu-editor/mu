bdf268d854ee3a51a3271d009f2aedc791f5037e
Tim Golden <mail@timgolden.me.uk>
2020-06-10 21:35:16 +0100

    Explicitly look out for an absence of wheels for the release we want

--- win_installer.py
+++ win_installer.py
@@ -36,7 +36,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
 import os
 import re
 import shutil
@@ -152,6 +151,8 @@ def pypi_wheels_in(requirements):
         print("-", requirement, end=" ")
         package = yarg.get(name)
         releases = package.release(version)
+        if not releases:
+            raise RuntimeError(f"No releases found for {package}")
         if any(r.package_type == "wheel" for r in releases):
             wheels.append(requirement)
             feedback = "ok"
