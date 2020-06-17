992c919a9742f4e7cba9116ad428342c2de622fa
Tim Golden <mail@timgolden.me.uk>
2020-06-17 13:38:13 +0100

    Appease flake8

--- mu/interface/panes.py
+++ mu/interface/panes.py
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import sys
-import site
 import os
 import re
 import platform
