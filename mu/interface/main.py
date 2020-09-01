9e1d26bce8adbc1262b5140a32097e8ec5c50390
Tim Golden <mail@timgolden.me.uk>
2020-09-01 05:56:19 +0100

    Small changes hoping to ease merge from master

--- mu/interface/main.py
+++ mu/interface/main.py
@@ -20,7 +20,7 @@ import sys
 import logging
 import os.path
 import serial
-from PyQt5.QtCore import QSize, Qt, pyqtSignal, QTimer, QIODevice
+from PyQt5.QtCore import QSize, Qt, pyqtSignal, QTimer
 from PyQt5.QtWidgets import (
     QToolBar,
     QAction,
--- mu/modes/base.py
+++ mu/modes/base.py
@@ -24,8 +24,8 @@ import time
 import logging
 import pkgutil
 from serial import Serial
-from PyQt5.QtSerialPort import QSerialPortInfo
-from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5.QtCore import QObject, pyqtSignal, QIODevice, QTimer
 from mu.logic import HOME_DIRECTORY, WORKSPACE_NAME, get_settings_path
 from mu.contrib import microfs
 
