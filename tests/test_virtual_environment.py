4dd2800789d785e0b9b46c34cfc7155bc7c35a62
Tim Golden <mail@timgolden.me.uk>
2020-09-03 13:29:24 +0100

    Refactor again to use a multi-patch test fixture

--- tests/test_virtual_environment.py
+++ tests/test_virtual_environment.py
@@ -18,6 +18,7 @@ to add or remove certain flags, or to use different wheels.
 import sys
 import os
 import contextlib
+import glob
 import pathlib
 import shutil
 import subprocess
@@ -36,6 +37,20 @@ WHEEL_FILENAME = "arrr-1.0.2-py3-none-any.whl"
 def venv_name():
     return uuid.uuid1().hex
 
+
+@pytest.fixture
+def patched():
+    """Creating a real venv on disk is expensive. Here we patch out
+    the expensive parts and pass them into the test so we can detect
+    how they've been called
+    """
+    with patch.object(subprocess, "run") as subprocess_run, \
+      patch.object(VE, "run_python") as run_python, \
+      patch.object(VE, "install_jupyter_kernel"), \
+      patch.object(VE, "find_interpreter"):
+        yield subprocess_run, run_python
+
+
 def test_create_virtual_environment_on_disk(tmp_path):
     """Ensure that we're actually creating a working virtual environment
     on the disk with wheels installed
@@ -48,6 +63,7 @@ def test_create_virtual_environment_on_disk(tmp_path):
     with patch.object(mu.virtual_environment, "wheels_dirpath", wheels_dirpath):
         venv = mu.virtual_environment.VirtualEnvironment(venv_dirpath)
         venv.create()
+        venv_site_packages = venv_dirpath / "lib" / "site-packages"
 
         #
         # Having a series of unrelated asserts is generally frowned upon
@@ -58,19 +74,35 @@ def test_create_virtual_environment_on_disk(tmp_path):
         # The remaining tests will mock things out and then just check that
         # the appropriate things were called
         #
+
+        #
+        # Check that we've created a virtual environment on disk
+        #
         assert (venv_dirpath / "pyvenv.cfg").is_file()
-        expected_pip_filepath = venv_dirpath / "lib" / "site-packages" / "pip"
+
+        #
+        # Check that we have an installed version of pip
+        #
+        expected_pip_filepath = venv_site_packages / "pip"
         pip_output = venv.pip("--version")
         assert str(expected_pip_filepath) in pip_output
-        venv.find_interpreter()
-        #
-        # FIXME: need to use platform-independent location
-        #
-        assert pathlib.Path(venv.interpreter) == venv_dirpath / "scripts" / "python.exe"
 
-@patch.object(subprocess, "run")
-@patch.object(VE, "run_python")
-def test_create_virtual_environment_path(mock_run, mock_run_python, venv_name, tmp_path):
+        #
+        # Check that a Python interpreter is found in the bin/scripts directory
+        #
+        venv.find_interpreter()
+        bin = "scripts" if sys.platform == "win32" else "bin"
+        assert pathlib.Path(venv.interpreter) == venv_dirpath / bin / "python.exe"
+
+        #
+        # Check that our test wheel has been installed to a single module
+        #
+        expected_result = str(venv_site_packages / "arrr.py")
+        result = venv.run_python("-c", "import arrr; print(arrr.__file__)").strip()
+        assert result == expected_result
+
+
+def test_create_virtual_environment_path(patched, tmp_path, venv_name):
     """Ensure a virtual environment object can be created by passing in
     a valid directory path.
     NB this doesn't create the venv itself; only the object
@@ -80,27 +112,23 @@ def test_create_virtual_environment_path(mock_run, mock_run_python, venv_name, t
     assert venv.path == dirpath
 
 
-@patch.object(subprocess, "run")
-@patch.object(VE, "run_python")
-def test_create_virtual_environment_name_obj(mock_run, mock_run_python, tmp_path, venv_name):
+def test_create_virtual_environment_name_obj(patched, venv_name):
     """Ensure a virtual environment object has a name.
     """
-    dirpath = tmp_path / venv_name
-    venv = mu.virtual_environment.VirtualEnvironment(dirpath)
+    venv = mu.virtual_environment.VirtualEnvironment(venv_name)
     assert venv.name == venv_name
 
 
-@patch.object(subprocess, "run")
-@patch.object(VE, "run_python")
-@patch.object(VE, "find_interpreter")
-#
-# FIXME: need to find some way of combining several patches into one fixture
-#
-def test_base_packages_installed(mock_run, mock_run_python, mock_find_interpreter, tmp_path, venv_name):
+def test_base_packages_installed(patched, venv_name):
     """Ensure that, when the venv is installed, the base packages are installed
     from wheels
     """
-    dirpath = tmp_path / venv_name
-    venv = mu.virtual_environment.VirtualEnvironment(dirpath)
+    _, run_python = patched
+    venv = mu.virtual_environment.VirtualEnvironment(venv_name)
     venv.create()
-    assert mock_run_python.called_with("pip")
+    #
+    # Check that we're calling `pip install` with all the wheels in our wheelhouse
+    #
+    expected_python_args = ("-m", "pip", "install") + tuple(glob.glob(os.path.join(mu.virtual_environment.wheels_dirpath, "*.whl")))
+    assert expected_python_args == run_python.call_args.args
+
