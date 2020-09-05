11446397f6c06c83d2c0e66e44e3e9c59d827b06
Tim Golden <mail@timgolden.me.uk>
2020-09-05 20:59:00 +0100

    Unversion local utility files

--- fresh-run.cmd
+++
@@ -1,5 +0,0 @@
-rmdir /s/q  %LOCALAPPDATA%\python\mu\mu_venv
-del %LOCALAPPDATA%\python\mu\Logs\mu.log
-del mu\wheels\*.whl
-.venv\scripts\python -mmu
-PAUSE
--- fresh-venv.cmd
+++
@@ -1,2 +0,0 @@
-IF EXIST .venv (rmdir /s/q .venv) && py -3 -mvenv .venv && .venv\scripts\python -mpip install --upgrade pip && .venv\scripts\pip install -e .[dev] && .venv\scripts\activate
-PAUSE
