4bb54b441ccc598ced9a024e09c6fab5d68ad4fe
Carlos Pereira Atencio <carlosperate@users.noreply.github.com>
2020-09-10 23:44:54 +0100

    Package: Add extra arm checks for environmental markers

--- setup.py
+++ setup.py
@@ -21,9 +21,12 @@ with open(os.path.join(base_dir, "CHANGES.rst"), encoding="utf8") as f:
 
 
 install_requires = [
-    'PyQt5==5.13.2;"arm" not in platform_machine',
-    'QScintilla==2.11.3;"arm" not in platform_machine',
-    'PyQtChart==5.13.1;"arm" not in platform_machine',
+    "PyQt5==5.13.2"
+    + ';"arm" not in platform_machine and "aarch" not in platform_machine',
+    "QScintilla==2.11.3"
+    + ';"arm" not in platform_machine and "aarch" not in platform_machine',
+    "PyQtChart==5.13.1"
+    + ';"arm" not in platform_machine and "aarch" not in platform_machine',
     # `flake8` is actually a testing/packaging dependency that, among other
     # packages, brings in `pycodestyle` and `pyflakes` which are runtime
     # dependencies. For the sake of "locality", it is being declared here,
