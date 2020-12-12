2207b5f19dd23de72edc0a7b51c8483ac53d766a
Tim Golden <mail@timgolden.me.uk>
2020-12-12 10:31:29 +0000

    No longer needed

--- _qt-trial/slowgen.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-import time
-import random
-
-
-for i in range(5):
-    print("".join(random.sample("abcdefghijklmnopqrstuvwxyz", random.randint(3, 10))))
-    time.sleep(10 * random.random())
-
