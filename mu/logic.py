a8d427f2324deb49176d4fcb241d66518432dab8
Tim Golden <mail@timgolden.me.uk>
2021-01-08 11:48:02 +0000

    Add version constraints for venv downloads (#1228)

--- mu/logic.py
+++ mu/logic.py
@@ -932,7 +932,6 @@ class Editor(QObject):
         # so it will not be focused over by another tab
         if paths and len(paths) > 0:
             self.load_cli(paths)
-        print("Checking tab_count:", self._view.tab_count)
         self.change_mode(self.mode)
         self.show_status_message(random.choice(MOTD), 10)
         if not self._view.tab_count:
--- mu/wheels/__init__.py
+++ mu/wheels/__init__.py
@@ -10,17 +10,12 @@ import subprocess
 logger = logging.getLogger(__name__)
 
 mode_packages = [
-    #
-    # There's a Windows compiler bug atm which means
-    # that numpy 1.19.4 will fail at runtime
-    #
-    ("numpy", "<1.19.4"),
     ("pgzero", ""),
     ("Flask", "==1.1.2"),
     ("pyserial", "==3.4"),
     ("qtconsole", "==4.7.4"),
     ("nudatus", ">=0.0.3"),
-    ("black", ">=19.10b0"),
+    ("black", '>=19.10b0;python_version>"3.5"'),
 ]
 WHEELS_DIRPATH = os.path.dirname(__file__)
 
@@ -29,8 +24,8 @@ def download(dirpath=WHEELS_DIRPATH):
     #
     # Download the wheels needed for modes
     #
-    print("download:WHEELS_DIRPATH", WHEELS_DIRPATH)
-    print("download:mode_packages", mode_packages)
+    logger.debug("WHEELS_DIRPATH: %s", WHEELS_DIRPATH)
+    logger.debug("mode_packages: %s", mode_packages)
     for package in mode_packages:
         print("download:package", package)
         subprocess.run(
@@ -64,19 +59,3 @@ def download(dirpath=WHEELS_DIRPATH):
                 check=True,
             )
             os.remove(filepath)
-
-    #
-    # FIXME: for now, we need numpy==1.19.3 as there's a Windows bug which
-    # prevents us from using >=1.19.4
-    # https://developercommunity.visualstudio.com/
-    # content/problem/1207405/
-    # fmod-after-an-update-to-windows-2004-is-causing-a.html
-    #
-    # We explicitly download 1.19.3 but then pygame pulls in 1.19.4 as one
-    # of its deps
-    #
-    # So, for now, we just find and delete the numpy 1.19.4 wheels
-    #
-    for numpy_1194 in glob.glob(os.path.join(dirpath, "numpy-1.19.4*.whl")):
-        logger.warn("Found numpy 1.19.4; deleting %s", numpy_1194)
-        os.remove(numpy_1194)
