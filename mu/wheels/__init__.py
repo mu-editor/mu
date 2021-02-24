f6721933889eb8e8a0bbbf29193b85ce6de329ad
Carlos Pereira Atencio <carlosperate@users.noreply.github.com>
2021-02-24 10:18:52 +0000

    Unpin pyserial version for the modes dependencies (#1329)
    * Switch to a wider pip reqt for pyserial as we're getting a clash between our ==3.4 version and the >=3.0 for esptool
    Mock out some ensure/create calls on venv
    
    * + Tidy-ups
    
    Co-authored-by: Tim Golden <mail@timgolden.me.uk>

--- mu/wheels/__init__.py
+++ mu/wheels/__init__.py
@@ -22,7 +22,7 @@ mode_packages = [
         "git+https://github.com/mu-editor/pgzero.git@mu-wheel",
     ),
     ("flask", "flask==1.1.2"),
-    ("serial", "pyserial==3.4"),
+    ("serial", "pyserial>=3.0"),
     ("qtconsole", "qtconsole==4.7.4"),
     ("nudatus", "nudatus>=0.0.3"),
     ("esptool", "esptool==3.*"),
