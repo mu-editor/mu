1bac3137b144037027be537aeff7388daac66458
Tim Golden <mail@timgolden.me.uk>
2021-06-06 11:02:37 +0100

    Test that the pip upgrade process works and raises an exception with the output included on failure

--- tests/virtual_environment/test_virtual_environment.py
+++ tests/virtual_environment/test_virtual_environment.py
@@ -320,6 +320,22 @@ def test_jupyter_kernel_installed(patched, venv):
             assert expected_jupyter_args == args[: len(expected_jupyter_args)]
 
 
+def test_upgrade_pip_failure(venv):
+    """Ensure that we raise an error with output when pip can't be upgraded"""
+    output = uuid.uuid1().hex
+    with mock.patch.object(venv, "run_subprocess", return_value=(True, output)):
+        venv.upgrade_pip()
+
+def test_upgrade_pip_success(venv):
+    """Ensure that we raise an error with output when pip can't be upgraded"""
+    output = uuid.uuid1().hex
+    with mock.patch.object(venv, "run_subprocess", return_value=(False, output)):
+        try:
+            venv.upgrade_pip()
+        except VEError as exc:
+            assert "nable to upgrade pip" in exc.message
+            assert output in exc.message
+
 def test_install_user_packages(patched, venv):
     """Ensure that, given a list of packages, we pip install them
 
