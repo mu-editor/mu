17dfee141f7187cadbe197d855f96290c3870714
Tim Golden <mail@timgolden.me.uk>
2021-06-13 20:30:37 +0100

    Detect change of mu version (#1624)
    Because a change of Mu version can bring with it updated wheels, we now detect a change of Mu version and recreate any runtime virtual environment.
    
    + Additional test coverage for the virtual environment module

--- tests/virtual_environment/test_process.py
+++ tests/virtual_environment/test_process.py
@@ -3,6 +3,7 @@
 Tests for the QProcess-based Process class
 """
 import sys
+import platform
 from unittest import mock
 import uuid
 
@@ -11,6 +12,10 @@ import pytest
 from PyQt5.QtCore import QTimer, QProcess
 
 from mu import virtual_environment
+
+is_arm = platform.machine().startswith("arm") or platform.machine().startswith(
+    "aarch"
+)
 
 
 def test_creation_environment():
@@ -43,6 +48,10 @@ def test_run_blocking():
     assert output == expected_output
 
 
+@pytest.mark.skipif(
+    is_arm,
+    reason="Unreliable on ARM probably because of slowness of test container",
+)
 def test_run_blocking_timeout():
     """Ensure that a process is run synchronously and times out"""
     p = virtual_environment.Process()
@@ -61,9 +70,9 @@ def test_run_blocking_timeout():
                 + expected_stdout
                 + "'); sys.stderr.write('"
                 + expected_stderr
-                + "'); time.sleep(2.0)",
+                + "'); time.sleep(1.0)",
             ],
-            wait_for_s=1.0,
+            wait_for_s=0.5,
         ).strip()
     except virtual_environment.VirtualEnvironmentError as exc:
         assert expected_stdout in exc.message
@@ -132,3 +141,21 @@ def test_wait_shows_failure():
         p.run(None, None)
         with pytest.raises(virtual_environment.VirtualEnvironmentError):
             p.wait()
+
+
+#
+# There are two places we return the outputs from subprocesses
+# decoded to utf-8 with errors replaced by the unicode "unknown" character:
+# The result of Process.wait (which can be invoked via .run_blocking); and
+# the result of VirtualEnvironment.run_subprocess
+#
+def test_run_blocking_invalid_utf8():
+    """Ensure that if the output of a QProcess run is not valid UTF-8 we
+    carry on as best we can"""
+    corrupted_utf8 = b"\xc2\x00\xa3"
+    expected_output = "\ufffd\x00\ufffd"
+    with mock.patch.object(QProcess, "readAll") as mocked_readAll:
+        mocked_readAll.return_value.data.return_value = corrupted_utf8
+        p = virtual_environment.Process()
+        output = p.run_blocking(sys.executable, ["-c", "print()"])
+    assert output == expected_output
