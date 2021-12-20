005a15f8e82a4a48f09a59777ed7fee3cbfafb9c
carlosperate <carlosperate@users.noreply.github.com>
2021-12-20 10:59:49 +0000

    Fixes #1922 - fix string for better translatability.

--- mu/logic.py
+++ mu/logic.py
@@ -573,7 +573,7 @@ class Device:
         if self.board_name:
             return self.board_name
         else:
-            return self.long_mode_name + _(" compatible")
+            return _("{} compatible").format(self.long_mode_name)
 
     def __eq__(self, other):
         """
