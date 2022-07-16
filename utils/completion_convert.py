db4091dba2cb5ef597a1a6e2934d9c2ed04595b6
Jeffrey04 <mycyberpet@yahoo.com>
2022-07-16 10:12:30 +0100

    mu #2291 writing generated code into a script

--- utils/completion_convert.py
+++ utils/completion_convert.py
@@ -23,34 +23,46 @@ def format_arguments(arguments):
 
 
 def main():
-    with open(sys.argv[1], "r") as f:
-        tree = ast.parse(f.read())
+    with open("test.py", "w") as fout:
+        result = []
 
-        for node in ast.iter_child_nodes(tree):
-            if isinstance(node, ast.FunctionDef):
-                docstring = ast.get_docstring(node, clean=True)
-                print(
-                    "{}{} {}".format(
-                        node.name,
-                        format_arguments(node.args),
-                        " ".join(docstring.split("\n")) if docstring else "",
-                    )
-                )
-            elif isinstance(node, ast.ClassDef):
-                for node_sub in node.body:
-                    if isinstance(node_sub, ast.FunctionDef) and (
-                        not node_sub.name.startswith("__")
-                        and not node_sub.name.endswith("__")
-                    ):
-                        docstring = ast.get_docstring(node_sub, clean=True)
-                        print(
-                            "{}.{}{} {}".format(
-                                node.name,
-                                node_sub.name,
-                                format_arguments(node_sub.args),
-                                " ".join(docstring.split("\n")) if docstring else "",
-                            )
+        with open(sys.argv[1], "r") as fin:
+            tree = ast.parse(fin.read())
+
+            for node in ast.iter_child_nodes(tree):
+                if isinstance(node, ast.FunctionDef):
+                    docstring = ast.get_docstring(node, clean=True)
+                    result.append(
+                        "{}{} {}".format(
+                            node.name,
+                            format_arguments(node.args),
+                            " ".join(docstring.split("\n")) if docstring else "",
                         )
+                    )
+                elif isinstance(node, ast.ClassDef):
+                    for node_sub in node.body:
+                        if isinstance(node_sub, ast.FunctionDef) and (
+                            not node_sub.name.startswith("__")
+                            and not node_sub.name.endswith("__")
+                        ):
+                            docstring = ast.get_docstring(node_sub, clean=True)
+                            result.append(
+                                "{}.{}{} {}".format(
+                                    node.name,
+                                    node_sub.name,
+                                    format_arguments(node_sub.args),
+                                    " ".join(docstring.split("\n"))
+                                    if docstring
+                                    else "",
+                                )
+                            )
+
+        # FIXME fix this hack with ast.unparse when version is bumped to >= 3.9
+        fout.write(
+            "MICROBIT_APIS = [{}]".format(
+                ", ".join(fr'_("{doc.strip()}")' for doc in result)
+            )
+        )
 
 
 if __name__ == "__main__":
