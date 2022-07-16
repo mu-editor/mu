86c45432f84b0c34cc4dd07eab62e3899b8750b3
Jeffrey04 <mycyberpet@yahoo.com>
2022-07-16 09:23:00 +0100

    mu #2291 code now works with arbitary module

--- utils/completion_convert.py
+++ utils/completion_convert.py
@@ -3,36 +3,54 @@ GitHub Issue #2291
 Convert python stub file into Qscintilla autocompletion files
 """
 
-import importlib.util
+import ast
 import sys
-from inspect import getmembers, isclass, isfunction, signature
-
-spec = importlib.util.spec_from_file_location("module", sys.argv[1])
-module = importlib.util.module_from_spec(spec)
-# sys.modules["testcode"] = module
-spec.loader.exec_module(module)
 
 
-def format_docstring(x):
-    return (
-        " ".join(sentence.strip() for sentence in x.__doc__.split("\n"))
-        if x.__doc__
-        else ""
-    )
+def format_arguments(arguments):
+    result = []
+
+    for arg in arguments.args:
+        result.append(f"{arg.arg}")
+
+    if arguments.vararg:
+        result.append(f"*{arguments.vararg.arg}")
+
+    if arguments.kwarg:
+        result.append(f"**{arguments.kwarg.arg}")
+
+    return f"({', '.join(result)})"
 
 
 def main():
-    print(
-        list(
-            f"{name}{str(signature(fn))} {format_docstring(fn)}".strip()
-            for name, fn in getmembers(module, isfunction)
-        )
-        + list(
-            f"{class_name}.{class_method}{str(signature(fn))} {format_docstring(fn)}".strip()
-            for class_name, _class in getmembers(module, isclass)
-            for class_method, fn in getmembers(_class, isfunction)
-        )
-    )
+    with open(sys.argv[1], "r") as f:
+        tree = ast.parse(f.read())
+
+        for node in ast.iter_child_nodes(tree):
+            if isinstance(node, ast.FunctionDef):
+                docstring = ast.get_docstring(node, clean=True)
+                print(
+                    "{}{} {}".format(
+                        node.name,
+                        format_arguments(node.args),
+                        " ".join(docstring.split("\n")) if docstring else "",
+                    )
+                )
+            elif isinstance(node, ast.ClassDef):
+                for node_sub in node.body:
+                    if isinstance(node_sub, ast.FunctionDef) and (
+                        not node_sub.name.startswith("__")
+                        and not node_sub.name.endswith("__")
+                    ):
+                        docstring = ast.get_docstring(node_sub, clean=True)
+                        print(
+                            "{}.{}{} {}".format(
+                                node.name,
+                                node_sub.name,
+                                format_arguments(node_sub.args),
+                                " ".join(docstring.split("\n")) if docstring else "",
+                            )
+                        )
 
 
 if __name__ == "__main__":
