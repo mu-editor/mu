c70ff4574202543d019b82c42ba11ad4df920238
haikusw <haikusw@gmail.com>
2022-12-25 14:18:05 -0800

    remove attempt to retain old logic, which realy doesn't make any sense, and just set minimum size

--- mu/interface/main.py
+++ mu/interface/main.py
@@ -1154,10 +1154,7 @@ class Window(QMainWindow):
         self.update_title()
         self.read_only_tabs = False
         screen_width, screen_height = self.screen_size()
-        self.setMinimumSize(
-            min(screen_width // 2, MIN_WINDOW_WIDTH),
-            min(screen_height // 2, MIN_WINDOW_HEIGHT),
-        )
+        self.setMinimumSize(MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT)
         self.setTabPosition(Qt.AllDockWidgetAreas, QTabWidget.North)
         self.widget = QWidget()
         widget_layout = QVBoxLayout()
