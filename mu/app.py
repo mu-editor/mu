d271253d976d8b7a3ad36616e0d8fff27c66bf3b
mbedNoobNinja <novoltage@gmail.com>
2023-04-20 14:14:26 +0300

    Allows Mu to work with VK micropython boards.

--- mu/app.py
+++ mu/app.py
@@ -59,6 +59,7 @@ from .modes import (
     LegoMode,
     PicoMode,
     SnekMode,
+    VkMode,
 )
 from .interface.themes import NIGHT_STYLE, DAY_STYLE, CONTRAST_STYLE
 from . import settings
@@ -254,6 +255,7 @@ def setup_modes(editor, view):
         "pygamezero": PyGameZeroMode(editor, view),
         "lego": LegoMode(editor, view),
         "pico": PicoMode(editor, view),
+        "vk": VkMode(editor, view),
     }
 
 
