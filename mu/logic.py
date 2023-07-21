948742c344f59c0b0e7d1c720fa98ad025d15811
Chung-Fan <airolg1111@gmail.com>
2023-07-21 11:42:25 +0200

    adding new MOTD line

--- mu/logic.py
+++ mu/logic.py
@@ -173,6 +173,7 @@ MOTD = [  # Candidate phrases for the message of the day (MOTD).
     _("Don't stare at a blank screen - try something out."),
     _("If you're stuck - try explaining it to a rubber duck."),
     _("It's ok to be stuck, go for a walk and then try again (:"),
+    _("Don't look backwards for very long. We keep moving forward, opening up new doors and doing new things, and curiosity keeps leading us down new paths."),
 ]
 
 NEWLINE = "\n"
