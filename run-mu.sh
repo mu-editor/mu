739fb24251ba47a287ecde8830a32a2d4bba937e
Tim Golden <mail@timgolden.me.uk>
2023-09-08 07:36:30 +0100

    Move short-term scripts out of the committed filespace

--- run-mu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export WAYLAND_DISPLAY=wayland-0
-#~ export XDG_RUNTIME_DIR=/tmp/1000-runtime-dir
-#export QT_QPA_PLATFORM=wayland
-export WAYLAND_DEBUG=1
-export PYTHONDEVMODE=1
-dist/Mu_Editor-1.2.1-x86_64.AppImage
