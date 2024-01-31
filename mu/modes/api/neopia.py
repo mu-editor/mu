5a33faf5e2717eeaba55eb2f1aa18f4d2d588615
Komron <knmv2311@gmail.com>
2024-01-31 11:25:36 +0500

    Added all tips

--- mu/modes/api/neopia.py
+++ mu/modes/api/neopia.py
@@ -30,6 +30,51 @@ NEOPIA_APIS = [
         "convert_scale(port, 0, 255, 0, 100) \nport: 'in1' ~ 'in3' \nparam2, param3 - min and max value of original scale: 0 ~ 255 \nparam3, param4 - min and max value of scale to be converted \nConvert a range of values from input port to another."
     ),
     _(
-
+        "color_check(port, color) \nport: 'in1' ~ 'in3' \ncolor: 'white', 'green', etc \nDetect color from input port."
     ),
+    _(
+        "led_on(port, brightness) \nport: 'out1' ~ 'out3', 'all' \npercentage of brightness: '0', '10' ~ '100' \nTurn on LED with brightness adjustment."
+    ),
+    _(
+        "led_off(port) \nport: 'out1' ~ 'out3', 'all' \nTurn off LED."
+    ),
+    _(
+        "color_led_on(port, 255, 0, 0) \nport: 'out1' ~ 'out3', 'all' \nparam1 - Red: 0 ~ 255 \nparam2 - Green: 0 ~ 255 \nparam3 - Blue: 0 ~ 255 \nTurn on color LED with color."
+    ),
+    _(
+        "motor_move(direction) \ndirection: 'forward', 'backward', 'left', 'right', 'stop' \nMove forward by motor."
+    ),
+    _(
+        "motor_rotate(motor, direction, speed) \nmotor: 'both', 'left', 'right' \ndirection: 'forward', 'backward', 'left', 'right', 'stop' \nspeed percentage or input port: '0', '10' ~ '100', 'in1' ~ 'in3' \nRotate DC motor by a count of motor, direction and speed."
+    ),
+    _(
+        "motor_stop('right') \nposition of motor: 'both', 'left', 'right' \nStop motor."
+    ),
+    _(
+        "buzzer(pitch, note, length) \npitch: '1' ~ '6' \nnote: 'c ', 'c#', 'db', 'd' ~ 'b' \nlength of note: '2', '4', '8', '16' \nPlay a sound by pitch, note with sharp and flat, and a length of note."
+    ),
+    _(
+        "buzzer_by_port(param) \nparam - port: 'in1' ~ 'in3' \nPlay a sound by value from input port."
+    ),
+    _(
+        "buzzer_stop() \nStop buzzer."
+    ),
+    _(
+        "get_angle(port) \nport: 'in1' ~ 'in3' \nGet a degree of an angle sensor."
+    ),
+    _(
+        "servo_rotate(port, direction, speed) \noutput port: 'out1', 'out2', 'out3' \ndirection: 'forward', 'backward' \nspeed percentage or input port: '0', '10' ~ '100', 'in1' ~ 'in3' \nRotate servo motor by direction and speed."
+    ),
+    _(
+        "servo_reset_degree(port) \noutput port: 'out1' ~ 'out3', 'all' \nMake 0 degree where servo motor is currently."
+    ),
+    _(
+        "servo_rotate_by_degree(port, direction, speed, angle) \noutput port: 'out1', 'out2', 'out3' \ndirection: 'forward', 'backward' \nspeed percentage or input port: '0', '10' ~ '100', 'in1' ~ 'in3' \ndegree of angle: '0', '5' ~ '180', 'in1' ~ 'in3' \nRotate servo motor by direction, speed and degree of angle."
+    ),
+    _(
+        "servo_stop(port) \noutput port: 'out1' ~ 'out3', 'all' \nStop servo motor."
+    ),
+    _(
+        "remote_button(button) \nbutton of RC: '1', '2', '3', 'up', 'left', 'right', 'down' \nCheck if the button of remote controller is pressed."
+    )
 ]
